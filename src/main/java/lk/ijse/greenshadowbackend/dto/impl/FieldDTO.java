fc2fcac2ce82b0b772788b69c9c37d152bd8ce7c
Madushanka24 <madushanka20021224@gmail.com>
2024-11-24 16:28:49 +0530

    field status and field dto/impl

---
+++ src/main/java/lk/ijse/greenshadowbackend/dto/impl/FieldDTO.java
@@ -0,0 +1,20 @@
+package lk.ijse.greenshadowbackend.dto.impl;
+import jakarta.persistence.*;
+import lk.ijse.greenshadowbackend.dto.FieldStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FieldDTO implements FieldStatus {
+   private String fieldCode;
+   private String fieldName;
+   private String location;
+   private String extent;
+   @Column(columnDefinition = "LONGTEXT")
+   private String fieldImage1;
+   @Column(columnDefinition = "LONGTEXT")
+   private String fieldImage2;
+}
