0a53450655c2eac4cac6e2e8bf780355067d3d59
Madushanka24 <madushanka20021224@gmail.com>
2024-11-30 10:30:27 +0530

    Equipment

---
+++ src/main/java/lk/ijse/greenshadowbackend/customStatusCode/SelectedEquipmentErrorStatus.java
@@ -0,0 +1,14 @@
+package lk.ijse.greenshadowbackend.customStatusCode;
+
+import lk.ijse.greenshadowbackend.dto.EquipmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SelectedEquipmentErrorStatus implements EquipmentStatus {
+    private int statusCode;
+    private String statusMessage;
+}
