f7d2c69789415d0fb6b5d83ba24213f6919af162
Madushanka24 <madushanka20021224@gmail.com>
2024-11-30 10:26:45 +0530

    crop

---
+++ src/main/java/lk/ijse/greenshadowbackend/customStatusCode/SelectedCropErrorStatus.java
@@ -0,0 +1,14 @@
+package lk.ijse.greenshadowbackend.customStatusCode;
+
+import lk.ijse.greenshadowbackend.dto.CropStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SelectedCropErrorStatus implements CropStatus {
+    private int statusCode;
+    private String statusMessage;
+}
