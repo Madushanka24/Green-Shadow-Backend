e55b85950df79a7c3d6b414ae128bd48168a868d
Madushanka24 <madushanka20021224@gmail.com>
2024-12-04 19:11:01 +0530

    Log Dao

---
+++ src/main/java/lk/ijse/greenshadowbackend/dao/LogDAO.java
@@ -0,0 +1,10 @@
+package lk.ijse.greenshadowbackend.dao;
+
+import lk.ijse.greenshadowbackend.entity.impl.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogDAO extends JpaRepository<Log, String> {
+
+}
