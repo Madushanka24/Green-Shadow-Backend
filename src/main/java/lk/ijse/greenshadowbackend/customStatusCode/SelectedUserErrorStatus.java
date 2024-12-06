8a1876a841e710cd9afac3f993733ce3b9369b54
Madushanka24 <madushanka20021224@gmail.com>
2024-12-06 10:29:04 +0530

    Auther User Controller

---
+++ src/main/java/lk/ijse/greenshadowbackend/customStatusCode/SelectedUserErrorStatus.java
@@ -0,0 +1,14 @@
+package lk.ijse.greenshadowbackend.customStatusCode;
+
+import lk.ijse.greenshadowbackend.dto.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SelectedUserErrorStatus implements UserStatus {
+    private int statusCode;
+    private String statusMessage;
+}
