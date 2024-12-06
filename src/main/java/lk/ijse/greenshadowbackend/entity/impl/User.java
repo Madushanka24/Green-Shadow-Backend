869946b595cda6ccdc39b42b5ea370d4a4e22ae3
Madushanka24 <madushanka20021224@gmail.com>
2024-12-06 10:33:14 +0530

    User Entity Update

--- src/main/java/lk/ijse/greenshadowbackend/entity/impl/User.java
+++ src/main/java/lk/ijse/greenshadowbackend/entity/impl/User.java
@@ -6,16 +6,36 @@ import lk.ijse.greenshadowbackend.entity.SuperEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
 @Table(name = "User")
-public class User implements SuperEntity {
+public class User implements UserDetails, SuperEntity {
     @Id
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+role.name()));
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
